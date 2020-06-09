02f7c55852d28ee145ead100a5f255b2a59da2a2
Pongsakorn Sommalai <bongtrop@gmail.com>
2020-06-09 23:31:37 +0700

    Update psgen __doc__

--- psgen/__init__.py
+++ psgen/__init__.py
@@ -2,6 +2,6 @@
 __version__ = '1.0.0'
 __name__ = "psgen"
 __doc__ = \
-"""A powershell payload generator tool for hacking"""
+"""psgen is a powershell payload generator tool for hacking. It can be used to generate, minify, and encode the powershell payload (e.g., reverse-shell, download-file, runas) to copyable text format."""
 
 from psgen.psgen import *
